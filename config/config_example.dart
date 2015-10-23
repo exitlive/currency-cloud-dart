715b14b89a4a8e3fdfc4127f45eb55a94c835299
queltos <queltos@gmail.com>
2015-10-23 19:07:04 +0200

    chore: rename class config to Config

--- config/config_example.dart
+++ config/config_example.dart
@@ -1,7 +1,7 @@
 // Copy this file to config.dart and put your values into the according
 // fields
 
-abstract class config {
+abstract class Config {
   static final loginId = 'apitester';
   static final apiKey = '99b0d6895f95e46d9eaf5c85aa0f64dca9007b7ab0778721b6cdc0a8bc7c56cf';
 }
