4db0e81683515e27764ab3786665f80e44265603
queltos <queltos@gmail.com>
2015-11-05 20:57:27 +0100

    fix: use get request for RatesApi.detailed

--- lib/src/rates_api.dart
+++ lib/src/rates_api.dart
@@ -3,7 +3,7 @@ part of currency_cloud;
 class RatesApi extends CurrencyCloudApi {
   RatesApi(authToken) : super(authToken);
 
-  Map<String, String> detailed(String buyCurrency, String sellCurrency, fixedSide, String amount,
+  Future<Map<String, String>> detailed(String buyCurrency, String sellCurrency, fixedSide, String amount,
       {String onBehalfOf, DateTime conversionDate}) async {
 
     var url = '/rates/detailed';
@@ -17,6 +17,6 @@ class RatesApi extends CurrencyCloudApi {
     if (onBehalfOf != null) body['on_behalf_of'] = onBehalfOf;
     if (conversionDate != null) body['conversion_date'] = conversionDate.toString();
 
-    return await client.post(url, body: body);
+    return await client.get(url, headers: body);
   }
 }
