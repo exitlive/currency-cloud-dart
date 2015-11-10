123f0000bc850b48ace4e467742cbe381c607ec5
queltos <queltos@gmail.com>
2015-11-10 21:54:50 +0100

    feat: add BeneficiariesApi.create()

--- lib/src/rates_api.dart
+++ lib/src/rates_api.dart
@@ -3,6 +3,7 @@ part of currency_cloud;
 class RatesApi extends CurrencyCloudApi {
   RatesApi(authToken) : super(authToken);
 
+  // TODO use package:Money instead of Strings
   Future<Map<String, String>> detailed(String buyCurrency, String sellCurrency, fixedSide, String amount) async {
     var url = '/rates/detailed';
 
