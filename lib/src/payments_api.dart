56878eea51eecb0188819d974e75874f75058b1d
queltos <queltos@gmail.com>
2015-11-10 22:50:34 +0100

    feat: implement PaymentsApi.create()

--- lib/src/payments_api.dart
+++ lib/src/payments_api.dart
@@ -4,6 +4,15 @@ class PaymentsApi extends CurrencyCloudApi {
   PaymentsApi(authToken) : super(authToken);
 
   Future<Map<String, String>> create(Money money, String beneficiaryId, String reason, String reference) {
-    return null;
+    var uri = '/payments/create';
+
+    var body = {};
+    body['amount'] = money.amountAsString;
+    body['currency'] = money.currency.code;
+    body['beneficiary_id'] = beneficiaryId;
+    body['reason'] = reason;
+    body['reference'] = reference;
+
+    return client.post(uri, body: body);
   }
 }
--- test/src/payments_api_test.dart
+++ test/src/payments_api_test.dart
@@ -1,6 +1,9 @@
 library currency_cloud.payments_api_test;
 
+import 'dart:async';
+
 import 'package:test/test.dart';
+import 'package:mockito/mockito.dart';
 
 import 'package:currency_cloud/currency_cloud.dart';
 import 'mock_client.dart';
@@ -18,7 +21,27 @@ main() {
       paymentsApi.client = mockClient;
     });
 
-    test('create() should call client.post() with correct body and return result', () {
+    test('create() should call client.post() with correct body and return result', () async {
+      var uri = '/payments/create';
+      var currencyISO = 'EUR';
+      Money money = new Money(5000, new Currency(currencyISO));
+      var beneficiaryId = 'funkyId';
+      var reason = 'somereason';
+      var reference = 'somereference';
+
+      var body = {};
+      body['currency'] = currencyISO;
+      body['amount'] = '50.00';
+      body['beneficiary_id'] = beneficiaryId;
+      body['reason'] = reason;
+      body['reference'] = reference;
+
+      var answer = {'custom': 'answer'};
+      when(mockClient.post(any, body: any)).thenReturn(new Future.value(answer));
+      var result = await paymentsApi.create(money, beneficiaryId, reason, reference);
+
+      verify(mockClient.post(uri, body: body)).called(1);
+      expect(result, answer);
     });
   });
 }
