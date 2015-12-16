d2b925e12deea8ef5854c838badc841e68056fee
queltos <queltos@gmail.com>
2015-12-16 18:03:42 +0100

    refac: replace redundant code in AuthenticateApi

--- lib/src/api/authenticate.dart
+++ lib/src/api/authenticate.dart
@@ -1,33 +1,16 @@
 part of currency_cloud;
 
 class AuthenticateApi extends CurrencyCloudApi {
-  String loginId, apiKey;
-
-  AuthenticateApi(this.loginId, this.apiKey, authToken) : super(authToken);
+  AuthenticateApi(client) : super(client);
 
   /// Authenticates this [CurrencyCloud] using given [loginId] and [apiKey]. This [CurrencyCloud] instance can
   /// only be used after authentication.
   authenticate() async {
-    // Save credentials for relogin in case our [AuthToken] expires
-    this.loginId = loginId;
-    this.apiKey = apiKey;
-
-    // Make sure we are in unauthenticated state
-    _authToken.reset;
-
-    var url = '/authenticate/api';
-    var body = {'login_id': loginId, 'api_key': apiKey,};
-
-    var response = await client.post(url, body: body);
-
-    _authToken.value = response['auth_token'];
+    client.authenticate();
   }
 
   /// Closes authenticated Session
   closeSession() async {
-    var url = '/authenticate/close_session';
-
-    await client.post(url);
-    _authToken.reset;
+    client.closeSession();
   }
 }
