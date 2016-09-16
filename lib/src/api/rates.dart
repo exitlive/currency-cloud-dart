43d5ac6258fecbf9779c5cae90162752352a818f
Martin Flucka <martin.flucka@gmail.com>
2016-09-16 12:59:28 +0200

    style: apply dartfmt

--- lib/src/api/rates.dart
+++ lib/src/api/rates.dart
@@ -7,7 +7,12 @@ class RatesApi extends CurrencyCloudApi {
   Future<Map<String, String>> detailed(String buyCurrency, String sellCurrency, fixedSide, String amount) async {
     var url = '/rates/detailed';
 
-    var body = {'buy_currency': buyCurrency, 'sell_currency': sellCurrency, 'fixed_side': fixedSide, 'amount': amount,};
+    var body = {
+      'buy_currency': buyCurrency,
+      'sell_currency': sellCurrency,
+      'fixed_side': fixedSide,
+      'amount': amount,
+    };
 
     return await client.get(url, body: body);
   }
